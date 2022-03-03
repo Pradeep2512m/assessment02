cafb75dc16837c4e2e9c8c416492d99965d12348
pradeep2512m <pradeepm6383@gmail.com>
2022-03-03 22:31:07 +0530

    programs

--- employee.py
+++ employee.py
@@ -1,5 +1,18 @@
-def employee_details():
-    name, age = "rahul", 25
-    designation, salary = "sofware engineer, 30000"
-    print("name: {}\nage: {}\ndesignation: {}\nsalary:".format(name, age, designation, salary))
-    employee_details()
+name = []
+age = []
+designation = []
+salary = []
+n = int(input("number of employee:"))
+for i in range(n):
+    name_1 = input("employee name:")
+    age_1 = input("employee age:")
+    designation_1 = input("employee designation:")
+    salary_1 = input("employee salary:")
+    name.append(name_1)
+    age.append(age_1)
+    designation.append(designation_1)
+    salary.append(salary_1)
+    print(name)
+    print(age)
+    print(designation)
+    print(salary)
