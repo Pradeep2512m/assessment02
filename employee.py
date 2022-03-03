ab18f0f27b0fa789a7b8c3862d46eafac783532a
pradeep2512m <pradeepm6383@gmail.com>
2022-03-03 21:38:32 +0530

    programs

---
+++ employee.py
@@ -0,0 +1,5 @@
+def employee_details():
+    name, age = "rahul", 25
+    designation, salary = "sofware engineer, 30000"
+    print("name: {}\nage: {}\ndesignation: {}\nsalary:".format(name, age, designation, salary))
+    employee_details()
